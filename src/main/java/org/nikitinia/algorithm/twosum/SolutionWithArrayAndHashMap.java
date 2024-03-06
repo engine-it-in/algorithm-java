2bd8bf3f09029cbbbe1a1083ff585d9719a86d43
engine-it-in <engineit@mail.ru>
2024-03-06 09:47:10 +0300

    Add second solutions;

--- src/main/java/org/nikitinia/algorithm/twosum/SolutionWithArrayAndHashMap.java
+++ src/main/java/org/nikitinia/algorithm/twosum/SolutionWithArrayAndHashMap.java
@@ -1,4 +1,26 @@
 package org.nikitinia.algorithm.twosum;
 
+import java.util.HashMap;
+
 public class SolutionWithArrayAndHashMap {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> mapNums = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int remainder = target - nums[i];
+
+            if (mapNums.containsKey(remainder)) {
+                return new int[]{mapNums.get(remainder), i};
+            } else {
+                mapNums.put(nums[i], i);
+            }
+        }
+
+        throw new IllegalArgumentException("no sum");
+
+    }
+
 }
