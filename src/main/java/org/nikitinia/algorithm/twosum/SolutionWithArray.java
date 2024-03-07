54aca3faed141d921436b29d7497dcca27a825c5
engine-it-in <engineit@mail.ru>
2024-03-07 09:13:31 +0300

    Add benchmark for algorithm;

--- src/main/java/org/nikitinia/algorithm/twosum/SolutionWithArray.java
+++ src/main/java/org/nikitinia/algorithm/twosum/SolutionWithArray.java
@@ -1,4 +1,20 @@
 package org.nikitinia.algorithm.twosum;
 
 public class SolutionWithArray {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i+1; j < nums.length; j++) {
+
+                if (nums[j] == target - nums[i]) {
+                        return new int[]{i, j};
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("no sum");
+    }
+
 }
--- src/main/java/org/nikitinia/algorithm/twosum/Verification.java
+++ src/main/java/org/nikitinia/algorithm/twosum/Verification.java
@@ -1,4 +1,29 @@
 package org.nikitinia.algorithm.twosum;
 
+import java.util.Arrays;
+
 public class Verification {
+
+    public static void main(String[] args) {
+
+        long[] timeBenchMark = new long[1000000];
+        int[] nums = new int[] {2,7,11,15};
+        int target = 9;
+
+        int[] resultOfSolutionWithArray = new int[0];
+        for (int i = 0; i < 1000000; i ++) {
+            long startTime = System.nanoTime();
+            resultOfSolutionWithArray = new SolutionWithArray().twoSum(nums, target);
+            long endTime = System.nanoTime();
+            timeBenchMark[i] = endTime-startTime;
+        }
+
+        Double valueBenchMark = Arrays.stream(timeBenchMark).average().getAsDouble();
+
+
+        System.out.println(Arrays.toString(resultOfSolutionWithArray));
+        System.out.println(String.format("Time is -> %4.3f MiliSec", (valueBenchMark)/1_000));
+
+
+    }
 }
